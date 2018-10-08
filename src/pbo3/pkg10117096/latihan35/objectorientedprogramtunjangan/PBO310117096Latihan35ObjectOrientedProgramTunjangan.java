938c73f43ee63762587d86ad13714e0ac1988dd4
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-10-08 22:42:55 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan35/objectorientedprogramtunjangan/PBO310117096Latihan35ObjectOrientedProgramTunjangan.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan35.objectorientedprogramtunjangan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program tunjangan karyawan
+ */
+public class PBO310117096Latihan35ObjectOrientedProgramTunjangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Karyawan kar = new Karyawan();
+
+        System.out.println("===========Program Tunjangan===========");
+        System.out.print("Berapa gaji pokok Anda perbulan ? : Rp. ");
+        kar.gajiPokok = scn.nextDouble();
+        System.out.print("Status Anda? (Menikah/Belum) :");
+        kar.status = scn.next();
+
+        kar.HasilHitung(kar.status,kar.gajiPokok);
+    }
+    
+}
